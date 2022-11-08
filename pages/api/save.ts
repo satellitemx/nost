ca53de450d8c462f4e85cf1598cfb7c0ccd16654
Siyuan You <satellitem@me.com>
2022-11-08 23:18:21 +1100

    parse JSON

--- pages/api/save.ts
+++ pages/api/save.ts
@@ -2,7 +2,7 @@ import db from "lib/data/db";
 import { NextApiHandler } from "next";
 
 const handler: NextApiHandler = async (req, res) => {
-	const data = req.body
+	const data = JSON.parse(req.body)
 	const saved = await db.save(data.note_id, data.content)
 	
 	return res.json(saved)
